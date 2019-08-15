0c64c31f444f3f0a431210055ff77eafd74754d8
Morpheus <51204692+Glaninor@users.noreply.github.com>
2019-08-15 19:04:56 +0800

    Update randomForest.R

--- randomForest.R
+++ randomForest.R
@@ -24,19 +24,21 @@ rf.test.y <- as.matrix(test[,2])
 
 rf.model <- randomForest(x=rf.train.x,y=rf.train.y,ntree=500,mtry=500,importance=TRUE,proximity=TRUE)
 print(rf.model)
+
 Call:
- randomForest(x = rf.train.x, y = rf.train.y, ntree = 500, mtry=500,       importance = TRUE,proximity = TRUE) 
+ randomForest(x = rf.train.x, y = rf.train.y, ntree = 500, mtry = 500,      importance = TRUE, proximity = TRUE) 
                Type of random forest: regression
                      Number of trees: 500
-No. of variables tried at each split: 425
+No. of variables tried at each split: 500
 
-          Mean of squared residuals: 0.2123665
-                    % Var explained: 14.64
+          Mean of squared residuals: 0.2124953
+                    % Var explained: 14.74
 
 rf.predict <- predict(rf.model,rf.test.x)
 rf.prediction <- prediction(rf.predict,rf.test.y)
 rf.prediction.auc <- performance(rf.prediction,'auc')
 rf.prediction.auc@y.values
+
 [[1]]
 [1] 0.7530366
 
